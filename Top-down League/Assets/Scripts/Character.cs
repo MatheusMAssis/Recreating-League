9d0d88a81f4af5381c39805859062dcb8652b641
Matheus Assis <assis.matheus1@gmail.com>
2019-10-27 21:38:31 -0300

    adding first features

---
+++ Top-down League/Assets/Scripts/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject
+{
+    public string characterName;
+    public Sprite characterImage;
+
+    public int horizontalOffset;
+    public int verticalOffset;
+}
---
+++ Top-down League/Assets/Scripts/CharacterDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterDisplay : MonoBehaviour
+{
+    public Character character;
+
+    public TextMeshProUGUI characterName;
+    public Image characterImage;
+
+    void Start()
+    {
+        characterName.GetComponent<TextMeshProUGUI>().text = character.characterName;
+        characterImage.GetComponent<Image>().sprite = character.characterImage;
+
+        characterImage.transform.position = new Vector2(characterImage.transform.position.x + character.horizontalOffset, characterImage.transform.position.y + character.verticalOffset);
+    }
+}
