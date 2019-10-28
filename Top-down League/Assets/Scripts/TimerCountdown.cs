9d99b40729de9607a26583e50d3f21b86375edc0
Matheus Assis <assis.matheus1@gmail.com>
2019-10-27 23:00:18 -0300

    champion select screen has all the basics components

---
+++ Top-down League/Assets/Scripts/TimerCountdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerCountdown : MonoBehaviour
+{
+    public int timerMax;
+
+    TextMeshProUGUI textMeshComponent;
+    float countdown;
+
+    void Start()
+    {
+        textMeshComponent = GetComponent<TextMeshProUGUI>();
+        textMeshComponent.text = timerMax.ToString();
+        countdown = timerMax;
+    }
+
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        textMeshComponent.text = Mathf.RoundToInt(countdown).ToString();
+    }
+}
