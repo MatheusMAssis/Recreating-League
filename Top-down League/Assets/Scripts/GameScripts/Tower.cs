c8f9f9edee1f1c166039153174a7e520e3b75161
Matheus Assis <assis.matheus1@gmail.com>
2019-11-12 23:42:10 -0300

    adding game scene

---
+++ Top-down League/Assets/Scripts/GameScripts/Tower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("Minion"))
+        {
+            if (target == null)
+            {
+                target = collision.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        target = null;
+    }
+
+    void Update()
+    {
+        
+    }
+}
