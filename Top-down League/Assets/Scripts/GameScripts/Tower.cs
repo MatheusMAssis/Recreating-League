93874f9d305687d1d067a63c22ecc42d3210ac5f
Matheus Assis <assis.matheus1@gmail.com>
2019-11-13 02:34:33 -0300

    .

--- Top-down League/Assets/Scripts/GameScripts/Tower.cs
+++ Top-down League/Assets/Scripts/GameScripts/Tower.cs
@@ -11,7 +11,7 @@ public class Tower : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") || collision.CompareTag("Minion"))
         {
